8a84f6ef1e284ea3cb1207d8083ed6a1ad6c4019
senorsyzygy <45519112+senorsyzygy@users.noreply.github.com>
2021-11-23 16:04:08 +0000

    Initial build of the website

---
+++ README.md
@@ -0,0 +1,1 @@
+Portfolio website to show off some of my projects and tell whoever is viewing a little bit about myself
---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById('emailSection').style.display = 'none'
+}
+function showEmail(){
+    document.getElementById('emailSection').style.display = 'block'
+}
