e8c94eee4792048844ae65b03da8ecf64a62357d
senorsyzygy <45519112+senorsyzygy@users.noreply.github.com>
2021-11-29 14:16:21 +0000

    added email link

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     document.getElementById('emailSection').style.display = 'none'
 }
+//not implemented yet
 function showEmail(){
     document.getElementById('emailSection').style.display = 'block'
 }
